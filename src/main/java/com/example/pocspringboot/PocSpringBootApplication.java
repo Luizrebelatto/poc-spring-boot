eca8af008d2783e542608677a85b8a419f45556f
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2025-05-18 20:27:08 -0300

    init project

---
+++ src/main/java/com/example/pocspringboot/PocSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.example.pocspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PocSpringBootApplication.class, args);
+    }
+
+}
